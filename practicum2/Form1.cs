3643ca12a8bc830eb18c210d3fe21d4f2b187a13
rubenshu <ruben.sikkens@student.hu.nl>
2022-01-18 18:32:30 +0100

    Add project files.

---
+++ practicum2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace practicum2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Random rnd = new Random();
+            InitializeComponent();
+
+            num1Text.Text = rnd.Next(1, 11).ToString();
+            num2Text.Text = rnd.Next(1, 11).ToString();
+            num3Text.Text = rnd.Next(1, 11).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(num1Text.Text);
+            int num2 = Int32.Parse(num2Text.Text);
+            int num3 = Int32.Parse(num3Text.Text);
+            
+            String output = MethodRunner.RunAllMethods(num1,num2,num3);
+            methodOutput.Text = output;
+
+            String output2 = LambdaRunner.RunAllMethods(num1,num2,num3);
+            lambdaOutput.Text = output2;
+
+            
+            string[] words = output.Split('\n');
+            string[] words2 = output2.Split('\n');
+            List<String> outputs1 = new List<String>();
+            List<String> outputs2 = new List<String>();
+            foreach (var word in words) {
+               
+                 outputs1.Add(word.Split('=').Last());
+              
+            }
+            foreach (var word in words2) {
+
+                outputs2.Add(word.Split('=').Last());
+            }
+            Debug.WriteLine(outputs1.ToString());
+            Debug.WriteLine(outputs2.ToString());
+
+            MessageBox.Show(outputs1.SequenceEqual(outputs2).ToString());
+        }
+    }
+}
---
+++ practicum2/LambdaRunner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practicum2
+{
+    class LambdaRunner
+    {
+        public static String RunAllMethods(int num1, int num2, int num3)
+        {
+            StringBuilder output = new StringBuilder();
+
+            Func<int, int> timesThree = x => 3 * x;
+            Func<int ,int ,int ,int> addNumbers = (x,y,z) => x + y + z;
+            Predicate<int> isEven = x => x % 2 == 0;
+            Func<int, String> num2String = x => {
+                switch (x) {
+                    case 0: return "zero";
+                    case 1: return "one";
+                    case 2: return "two";
+                    case 3: return "three";
+                    case 4: return "four";
+                    case 5: return "five";
+                    case 6: return "six";
+                    case 7: return "seven";
+                    case 8: return "eight";
+                    case 9: return "nine";
+                    default: return "this shouldn't happen";
+                }
+            };
+            Action<Person> resetName = x => x.Name = "null";
+            Func<int, int, int, bool> inBetween = (x, y, z) => (x < y && y < z) || (z < y && y < x);
+            output.AppendFormat("TimesThree({0}) = {1}\n", num1, timesThree(num1));
+            output.AppendFormat("AddNumbers({0},{1},{2}) = {3}\n", num1, num2, num3, addNumbers(num1, num2, num3));
+            output.AppendFormat("IsEven({0}) = {1}\n", num1, isEven(num1));
+            output.AppendFormat("Num2String({0}) = {1}\n", num1, num2String(num1));
+            output.AppendFormat("InBetween({0},{1},{2}) = {3}\n", num1, num2, num3, inBetween(num1, num2, num3));
+            Person p = new Person { Name = "Jan" };
+            resetName(p);
+            output.AppendFormat("ResetName, daarna (Name == null) = {0}\n", p.Name=="null");
+
+          
+
+            return output.ToString();
+        }
+
+    }
+}
